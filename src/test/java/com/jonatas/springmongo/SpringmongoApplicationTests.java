c092023e27e5489bf2210ff8c1477890b8e52eb5
Jonatas Vasconcelos <j.olivasc@outlook.com>
2021-03-07 02:40:40 -0300

    criação do projeto

---
+++ src/test/java/com/jonatas/springmongo/SpringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonatas.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
