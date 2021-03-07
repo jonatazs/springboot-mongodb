eaa955f1a3e66a3e9ecedde72ee2ec6d4683a719
Jonatas Vasconcelos <j.olivasc@outlook.com>
2021-03-07 11:09:19 -0300

    metodo post

---
+++ src/main/java/com/jonatas/springmongo/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.jonatas.springmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jonatas.springmongo.domain.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		User maria = new User("1", "Mari silva", "maria@gmail");
+		User alex = new User("1", "Joaquim", "joadosquim@gmail");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria, alex));
+		return ResponseEntity.ok().body(list);
+	}	
+}
